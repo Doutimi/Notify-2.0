5ce2c9e11cfa9b7a2e0a3a908feeee8f220aa54a
Doutimi <isenahdoutimiebi@gmail.com>
2024-09-24 10:30:58 +0100

    Editing the header component to reflect active tabs

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,19 +1,31 @@
 type HeaderProps = {
-  link: string;
+  // link: string;
+  activeTab:"bills"|"appointments"|"home"
 };
 
-export function Header({ link }: HeaderProps) {
+export function Header({ activeTab }: HeaderProps) {
+  let homeHref=activeTab!=="home"?"/":undefined
+  let billsHref=activeTab!=="bills"?"/bills":undefined
+  let appointmentsHref=activeTab!=="appointments"?"/appointments":undefined
+  
+  const activeClassName="bg-[#FDBF8B]"
   return (
     <header>
       <nav>
         <ul>
-          <li>
-            <a href="/">Home</a>
+          <li className={activeTab=="home"?activeClassName:undefined}>
+            <a href={homeHref} >
+                Home
+            </a>
           </li>
-          <li>
-            <a href={link}>Bills</a>
+          <li className={activeTab=="bills"?activeClassName:undefined}>
+            <a href={billsHref}>
+                Bills
+            </a>
           </li>
-          <li className="bg-[#FDBF8B]">Appointments</li>
+          <li className={activeTab=="appointments"?activeClassName:undefined} >
+            <a href={appointmentsHref}>Appointments</a>
+          </li>
         </ul>
       </nav>
     </header>
